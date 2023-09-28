85e1c0e2db36deb75a70e01902717eae57d84175
Victor Teo <victor.teo@fillmula.com>
2023-09-29 01:53:55 +0800

    add resolve function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+use crate::ast::schema::Schema;
+use crate::diagnostics::diagnostics::Diagnostics;
+
 pub mod ast;
 pub mod parser;
 pub mod resolver;
 pub mod diagnostics;
 pub mod utils;
+
+pub fn parse(main: impl AsRef<str>) -> (Schema, Diagnostics) {
+    let (schema, mut diagnostics) = parser::parse::parse(main);
+    resolver::resolve::resolve(&schema, &mut diagnostics);
+    (schema, diagnostics)
+}
---
+++ src/resolver/resolve.rs
@@ -0,0 +1,6 @@
+use crate::ast::schema::Schema;
+use crate::diagnostics::diagnostics::Diagnostics;
+
+pub(crate) fn resolve(schema: &Schema, diagnostics: &mut Diagnostics) {
+
+}
