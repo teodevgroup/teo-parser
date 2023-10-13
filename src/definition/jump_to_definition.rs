d294bb59ec8665f392967a5d898ec5a4df36ed0a
Victor Teo <victor.teo@fillmula.com>
2023-10-14 02:07:58 +0800

    update

--- src/definition/jump_to_definition.rs
+++ src/definition/jump_to_definition.rs
@@ -3,18 +3,18 @@ use crate::ast::schema::Schema;
 use crate::ast::span::Span;
 use crate::ast::top::Top;
 use crate::definition::definition::Definition;
+use crate::definition::jump_to_definition_in_import::jump_to_definition_in_import;
 use crate::definition::jump_to_definition_in_model::jump_to_definition_in_model;
 use crate::search::search_top::search_top;
 
 pub fn jump_to_definition(schema: &Schema, file_path: &str, line_col: (usize, usize)) -> Vec<Definition> {
     if let Some(source) = schema.source_at_path(file_path) {
         if let Some(top) = search_top(schema, file_path, line_col) {
-            match top {
-                Top::Model(m) => {
-                    return jump_to_definition_in_model(schema, source, m, line_col);
-                }
-                _ => ()
-            }
+            return match top {
+                Top::Model(m) => jump_to_definition_in_model(schema, source, m, line_col),
+                Top::Import(i) => jump_to_definition_in_import(schema, source, i, line_col),
+                _ => vec![]
+            };
         }
     }
     vec![]
--- src/definition/jump_to_definition_in_import.rs
+++ src/definition/jump_to_definition_in_import.rs
@@ -1,18 +1,24 @@
-// pub(crate) fn jump_to_definition(&self, context: &DefinitionContext, line_col: (usize, usize)) -> Vec<Definition> {
-//     if self.source.span.contains_line_col(line_col) {
-//         if !self.file_path.starts_with("(builtin)") {
-//             vec![
-//                 Definition {
-//                     path: self.file_path.clone(),
-//                     selection_span: self.source.span,
-//                     target_span: Span::default(),
-//                     identifier_span: Span::default(),
-//                 }
-//             ]
-//         } else {
-//             vec![]
-//         }
-//     } else {
-//         vec![]
-//     }
-// }
+use crate::ast::import::Import;
+use crate::ast::schema::Schema;
+use crate::ast::source::Source;
+use crate::ast::span::Span;
+use crate::definition::definition::Definition;
+
+pub(super) fn jump_to_definition_in_import(schema: &Schema, source: &Source, import: &Import, line_col: (usize, usize)) -> Vec<Definition> {
+    if import.source.span.contains_line_col(line_col) {
+        if !import.file_path.starts_with("(builtin)") {
+            vec![
+                Definition {
+                    path: import.file_path.clone(),
+                    selection_span: import.source.span,
+                    target_span: Span::default(),
+                    identifier_span: Span::default(),
+                }
+            ]
+        } else {
+            vec![]
+        }
+    } else {
+        vec![]
+    }
+}
