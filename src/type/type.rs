a57ef9c7905bd93966d2d9ad77d63070e27dd624
Victor Teo <victor.teo@fillmula.com>
2023-10-13 17:58:36 +0800

    update if and add validate

--- src/type/type.rs
+++ src/type/type.rs
@@ -586,7 +586,7 @@ impl Type {
             Type::ModelDirectRelations(path, _) => passed.is_model_direct_relations() && passed.as_model_direct_relations().unwrap().0.test(path),
             Type::FieldType(path, field) => passed.is_field_type() && path.test(passed.as_field_type().unwrap().0) && field.test(passed.as_field_type().unwrap().1),
             Type::FieldReference(s) => passed.is_field_reference() && s == passed.as_field_reference().unwrap(),
-            Type::GenericItem(identifier) => passed.is_generic_item() && passed.as_generic_item().unwrap() == identifier.as_str(),
+            Type::GenericItem(identifier) => true,
             Type::Keyword(k) => passed.is_keyword() && k == passed.as_keyword().unwrap(),
             Type::Optional(inner) => passed.is_null() || inner.test(passed) || (passed.is_optional() && inner.test(passed.as_optional().unwrap())),
             Type::Pipeline((a, b)) => passed.is_pipeline() && a.test(passed.as_pipeline().unwrap().0) && b.test(passed.as_pipeline().unwrap().1),
