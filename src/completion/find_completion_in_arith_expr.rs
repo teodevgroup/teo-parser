2c9a0a941ca532cdca8ef03de4b0cc4e917505fb
Victor Teo <victor.teo@fillmula.com>
2023-11-06 06:04:57 +0800

    find completion in expression containers

--- src/completion/find_completion_in_arith_expr.rs
+++ src/completion/find_completion_in_arith_expr.rs
@@ -2,7 +2,27 @@ use crate::ast::arith::ArithExpr;
 use crate::ast::schema::Schema;
 use crate::ast::source::Source;
 use crate::completion::completion_item::CompletionItem;
+use crate::completion::find_completion_in_expression::find_completion_in_expression;
 
 pub(super) fn find_completion_in_arith_expr(schema: &Schema, source: &Source, arith_expr: &ArithExpr, line_col: (usize, usize), namespace_path: &Vec<&str>) -> Vec<CompletionItem> {
-    vec![]
+    match arith_expr {
+        ArithExpr::Expression(e) => find_completion_in_expression(schema, source, e.as_ref(), line_col, namespace_path),
+        ArithExpr::UnaryOp(u) => if u.rhs.span().contains_line_col(line_col) {
+            find_completion_in_arith_expr(schema, source, u.rhs.as_ref(), line_col, namespace_path)
+        } else {
+            vec![]
+        }
+        ArithExpr::BinaryOp(b) => if b.lhs.span().contains_line_col(line_col) {
+            find_completion_in_arith_expr(schema, source, b.lhs.as_ref(), line_col, namespace_path)
+        } else if b.rhs.span().contains_line_col(line_col) {
+            find_completion_in_arith_expr(schema, source, b.rhs.as_ref(), line_col, namespace_path)
+        } else {
+            vec![]
+        }
+        ArithExpr::UnaryPostfixOp(p) => if p.lhs.span().contains_line_col(line_col) {
+            find_completion_in_arith_expr(schema, source, p.lhs.as_ref(), line_col, namespace_path)
+        } else {
+            vec![]
+        }
+    }
 }
--- src/completion/find_completion_in_dictionary_literal.rs
+++ src/completion/find_completion_in_dictionary_literal.rs
@@ -2,7 +2,16 @@ use crate::ast::literals::{DictionaryLiteral};
 use crate::ast::schema::Schema;
 use crate::ast::source::Source;
 use crate::completion::completion_item::CompletionItem;
+use crate::completion::find_completion_in_expression::find_completion_in_expression;
 
 pub(super) fn find_completion_in_dictionary_literal(schema: &Schema, source: &Source, dictionary_literal: &DictionaryLiteral, line_col: (usize, usize), namespace_path: &Vec<&str>) -> Vec<CompletionItem> {
+    for (key_expression, value_expression) in &dictionary_literal.expressions {
+        if key_expression.span().contains_line_col(line_col) {
+            return find_completion_in_expression(schema, source, key_expression, line_col, namespace_path);
+        }
+        if value_expression.span().contains_line_col(line_col) {
+            return find_completion_in_expression(schema, source, value_expression, line_col, namespace_path);
+        }
+    }
     vec![]
 }
