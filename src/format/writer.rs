b582e7ec6c404421dfc5dc9fc1363a793a4ca0aa
Victor Teo <victor.teo@fillmula.com>
2023-11-15 07:28:03 +0800

    writer lock

--- src/format/writer.rs
+++ src/format/writer.rs
@@ -8,6 +8,7 @@ use crate::traits::write::Write;
 pub struct Writer<'a> {
     pub(super) preferences: Preferences,
     pub(super) commands: Vec<Command<'a>>,
+    can_write: bool,
 }
 
 impl Default for Writer<'_> {
@@ -22,21 +23,37 @@ impl<'a> Writer<'a> {
         Self {
             preferences,
             commands: vec![],
+            can_write: true,
         }
     }
 
     pub fn write_children(&'a mut self, node: &'a dyn Write, children: Values<usize, Node>) {
+        if !self.can_write {
+            panic!("writer can only write only once in one call");
+        }
         let mut writer = Self::new(self.preferences);
-        children.for_each(|c| c.write(&mut writer));
+        children.for_each(|c| {
+            c.write(&mut writer);
+            writer.can_write = true;
+        });
         self.commands.push(Command::branch(node, writer.commands));
+        self.can_write = false;
     }
 
     pub fn write_content(&'a mut self, node: &'a dyn Write, content: &'a str) {
+        if !self.can_write {
+            panic!("writer can only write only once in one call");
+        }
         self.commands.push(Command::leaf(node, vec![content.as_ref()]));
+        self.can_write = false;
     }
 
     pub fn write_contents(&mut self, node: &dyn Write, contents: Vec<&'a str>) {
+        if !self.can_write {
+            panic!("writer can only write only once in one call");
+        }
         self.commands.push(Command::leaf(node, contents));
+        self.can_write = false;
     }
 
     pub fn flush(&self) -> String {
