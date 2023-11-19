f4539d6bd30dabc431c7dfc4efff76bac9264259
Victor Teo <victor.teo@fillmula.com>
2023-11-19 09:12:15 +0800

    rewrite resolve unit

--- src/resolver/resolve_unit.rs
+++ src/resolver/resolve_unit.rs
@@ -14,12 +14,12 @@ use crate::resolver::resolve_argument_list::{resolve_argument_list};
 use crate::resolver::resolve_expression::resolve_expression;
 use crate::resolver::resolve_interface_shapes::calculate_generics_map;
 use crate::resolver::resolver_context::ResolverContext;
-use crate::search::search_identifier_path::search_identifier_path_names_with_filter_to_expr_info;
 use crate::traits::named_identifiable::NamedIdentifiable;
 use crate::traits::node_trait::NodeTrait;
 use crate::traits::resolved::{Resolve, ResolveAndClone};
 use crate::utils::top_filter::top_filter_for_reference_type;
 use crate::expr::{ExprInfo, ReferenceInfo, ReferenceType};
+use crate::resolver::resolve_identifier::resolve_identifier_path_names_with_filter_to_expr_info;
 
 pub(super) fn resolve_unit<'a>(
     unit: &'a Unit,
@@ -58,18 +58,18 @@ fn resolve_current_item_for_unit<'a>(
         if let Some(reference_info) = current.reference_info() {
             match reference_info.r#type() {
                 ReferenceType::Config => resolve_config_reference_for_unit(reference_info.reference(), expression, context),
-                ReferenceType::DictionaryField => {} // should go to type resolving
-                ReferenceType::Constant => {} // should go to type resolving
+                ReferenceType::DictionaryField => resolve_current_item_type_for_unit(last_span, current, expression, context, keywords_map),
+                ReferenceType::Constant => resolve_current_item_type_for_unit(last_span, current, expression, context, keywords_map),
                 ReferenceType::Enum => resolve_enum_reference_for_unit(reference_info.reference(), expression, context),
-                ReferenceType::EnumMember => {} // should go to type resolving
+                ReferenceType::EnumMember => resolve_current_item_type_for_unit(last_span, current, expression, context, keywords_map),
                 ReferenceType::Model => resolve_model_reference_for_unit(reference_info.reference(), expression, context),
-                ReferenceType::ModelField => {}
+                ReferenceType::ModelField => ExprInfo::undetermined(),
                 ReferenceType::Interface => resolve_interface_reference_for_unit(reference_info.reference(), reference_info.generics().unwrap_or(&vec![]), expression, context),
-                ReferenceType::InterfaceField => {}
+                ReferenceType::InterfaceField => ExprInfo::undetermined(),
                 ReferenceType::Middleware => resolve_middleware_reference_for_unit(last_span.unwrap(), reference_info.reference(), expression, context),
-                ReferenceType::DataSet => unreachable!(),
-                ReferenceType::DecoratorDeclaration => unreachable!(),
-                ReferenceType::PipelineItemDeclaration => unreachable!(),
+                ReferenceType::DataSet => ExprInfo::undetermined(),
+                ReferenceType::DecoratorDeclaration => ExprInfo::undetermined(),
+                ReferenceType::PipelineItemDeclaration => ExprInfo::undetermined(),
                 ReferenceType::StructDeclaration => resolve_struct_reference_for_unit(last_span.unwrap(), reference_info.reference(), reference_info.generics().unwrap_or(&vec![]), expression, context),
                 ReferenceType::StructStaticFunction => resolve_struct_static_function_reference_for_unit(last_span.unwrap(), reference_info.reference(), reference_info.generics().unwrap_or(&vec![]), expression, context),
                 ReferenceType::StructInstanceFunction => resolve_struct_instance_function_reference_for_unit(last_span.unwrap(), reference_info.reference(), reference_info.generics().unwrap_or(&vec![]), expression, context),
@@ -77,36 +77,46 @@ fn resolve_current_item_for_unit<'a>(
                 ReferenceType::Namespace => resolve_namespace_reference_for_unit(reference_info.reference().string_path(), expression, context),
             }
         } else {
-            match current.r#type() {
-                Type::Optional(inner) => {
-                    context.insert_diagnostics_error(expression.span(), "expression might be null");
-                    resolve_current_item_for_unit(last_span, Some(&current.type_altered(inner.as_ref().clone())), expression, context, keywords_map)
-                }
-                Type::Null => resolve_builtin_struct_instance_for_unit("Null", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Bool => resolve_builtin_struct_instance_for_unit("Bool", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Int => resolve_builtin_struct_instance_for_unit("Int", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Int64 => resolve_builtin_struct_instance_for_unit("Int64", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Float32 => resolve_builtin_struct_instance_for_unit("Float32", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Float => resolve_builtin_struct_instance_for_unit("Float", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Decimal => resolve_builtin_struct_instance_for_unit("Decimal", &vec![], current, last_span, expression, context, keywords_map),
-                Type::String => resolve_builtin_struct_instance_for_unit("String", &vec![], current, last_span, expression, context, keywords_map),
-                Type::ObjectId => resolve_builtin_struct_instance_for_unit("ObjectId", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Date => resolve_builtin_struct_instance_for_unit("Date", &vec![], current, last_span, expression, context, keywords_map),
-                Type::DateTime => resolve_builtin_struct_instance_for_unit("DateTime", &vec![], current, last_span, expression, context, keywords_map),
-                Type::File => resolve_builtin_struct_instance_for_unit("File", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Regex => resolve_builtin_struct_instance_for_unit("Regex", &vec![], current, last_span, expression, context, keywords_map),
-                Type::Array(inner) => resolve_builtin_struct_instance_for_unit("Array", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
-                Type::Dictionary(inner) => resolve_builtin_struct_instance_for_unit("Dictionary", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
-                Type::Tuple(types) => resolve_tuple_for_unit(types, current, expression, context),
-                Type::Range(inner) => resolve_builtin_struct_instance_for_unit("Range", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
-                Type::EnumVariant(_) => resolve_enum_variant_for_unit(last_span.unwrap(), current, expression, context),
-                Type::InterfaceObject(reference, types) => resolve_interface_object_for_unit(reference, current, types, expression, context),
-                Type::StructObject(reference, types) => resolve_struct_object_for_unit(reference, types, expression, context),
-                _ => ExprInfo::undetermined(),
-            }
+            resolve_current_item_type_for_unit(last_span, current, expression, context, keywords_map)
         }
     } else {
         resolve_expression(expression, context, &expected, keywords_map)
+    }
+}
+
+fn resolve_current_item_type_for_unit<'a>(
+    last_span: Option<Span>,
+    current: &ExprInfo,
+    expression: &'a Expression,
+    context: &'a ResolverContext<'a>,
+    keywords_map: &BTreeMap<Keyword, Type>,
+) -> ExprInfo {
+    match current.r#type() {
+        Type::Optional(inner) => {
+            context.insert_diagnostics_error(expression.span(), "expression might be null");
+            resolve_current_item_for_unit(last_span, Some(&current.type_altered(inner.as_ref().clone())), expression, context, keywords_map)
+        }
+        Type::Null => resolve_builtin_struct_instance_for_unit("Null", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Bool => resolve_builtin_struct_instance_for_unit("Bool", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Int => resolve_builtin_struct_instance_for_unit("Int", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Int64 => resolve_builtin_struct_instance_for_unit("Int64", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Float32 => resolve_builtin_struct_instance_for_unit("Float32", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Float => resolve_builtin_struct_instance_for_unit("Float", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Decimal => resolve_builtin_struct_instance_for_unit("Decimal", &vec![], current, last_span, expression, context, keywords_map),
+        Type::String => resolve_builtin_struct_instance_for_unit("String", &vec![], current, last_span, expression, context, keywords_map),
+        Type::ObjectId => resolve_builtin_struct_instance_for_unit("ObjectId", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Date => resolve_builtin_struct_instance_for_unit("Date", &vec![], current, last_span, expression, context, keywords_map),
+        Type::DateTime => resolve_builtin_struct_instance_for_unit("DateTime", &vec![], current, last_span, expression, context, keywords_map),
+        Type::File => resolve_builtin_struct_instance_for_unit("File", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Regex => resolve_builtin_struct_instance_for_unit("Regex", &vec![], current, last_span, expression, context, keywords_map),
+        Type::Array(inner) => resolve_builtin_struct_instance_for_unit("Array", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
+        Type::Dictionary(inner) => resolve_builtin_struct_instance_for_unit("Dictionary", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
+        Type::Tuple(types) => resolve_tuple_for_unit(types, current, expression, context),
+        Type::Range(inner) => resolve_builtin_struct_instance_for_unit("Range", &vec![inner.as_ref()], current, last_span, expression, context, keywords_map),
+        Type::EnumVariant(_) => resolve_enum_variant_for_unit(last_span.unwrap(), current, expression, context),
+        Type::InterfaceObject(reference, types) => resolve_interface_object_for_unit(reference, current, types, expression, context),
+        Type::StructObject(reference, types) => resolve_struct_object_for_unit(reference, types, expression, context),
+        _ => ExprInfo::undetermined(),
     }
 }
 
@@ -635,13 +645,14 @@ fn resolve_namespace_reference_for_unit(
         ExpressionKind::Identifier(identifier) => {
             let mut names: Vec<&str> = string_path.iter().map(AsRef::as_ref).collect();
             names.push(identifier.name());
-            if let Some(result) = search_identifier_path_names_with_filter_to_expr_info(
+            if let Some(result) = resolve_identifier_path_names_with_filter_to_expr_info(
                 &names,
                 context.schema,
                 context.source(),
                 &context.current_namespace().map_or(vec![], |n| n.str_path()),
                 &top_filter_for_reference_type(ReferenceSpace::Default),
                 context.current_availability(),
+                context,
             ) {
                 result
             } else {
