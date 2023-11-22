c55c289e8bb8d36e43dee112fb2e4e4f8da91a71
Victor Teo <victor.teo@fillmula.com>
2023-11-22 17:02:58 +0800

    add more tests

--- src/completion/find_completion_in_array_literal.rs
+++ src/completion/find_completion_in_array_literal.rs
@@ -4,6 +4,7 @@ use crate::ast::schema::Schema;
 use crate::ast::source::Source;
 use crate::completion::completion_item::CompletionItem;
 use crate::completion::find_completion_in_expression::find_completion_in_expression;
+use crate::r#type::Type;
 use crate::traits::node_trait::NodeTrait;
 
 pub(super) fn find_completion_in_array_literal(schema: &Schema, source: &Source, array_literal: &ArrayLiteral, line_col: (usize, usize), namespace_path: &Vec<&str>, availability: Availability) -> Vec<CompletionItem> {
--- tests/auto_completion/enum_variant_literal/mod.rs
+++ tests/auto_completion/enum_variant_literal/mod.rs
@@ -9,4 +9,22 @@ mod test {
         let completions = auto_complete_items(&schema, path, (22, 14));
         assert_eq!(completions.len(), 2);
     }
+
+    #[test]
+    fn completion_items_for_through_fields() {
+        let path_buf = std::env::current_dir().unwrap().join("tests/auto_completion/enum_variant_literal/schemas/02.teo");
+        let path = path_buf.to_str().unwrap();
+        let (schema, _) = parse(path, None, None);
+        let completions = auto_complete_items(&schema, path, (53, 40));
+        println!("{:?}", completions);
+    }
+
+    #[test]
+    fn completion_items_for_through_fields_should_not_crash() {
+        let path_buf = std::env::current_dir().unwrap().join("tests/auto_completion/enum_variant_literal/schemas/03.teo");
+        let path = path_buf.to_str().unwrap();
+        let (schema, _) = parse(path, None, None);
+        let completions = auto_complete_items(&schema, path, (53, 37));
+        println!("{:?}", completions);
+    }
 }
