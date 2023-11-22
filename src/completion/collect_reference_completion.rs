6d5ab60c678905d2e5e51295ba54b794561e6113
Victor Teo <victor.teo@fillmula.com>
2023-11-22 15:18:39 +0800

    fix auto completion ignores availability bug

--- src/completion/collect_reference_completion.rs
+++ src/completion/collect_reference_completion.rs
@@ -22,7 +22,7 @@ fn collect_reference_completion_in_source_internal<'a>(schema: &'a Schema, sourc
         let mut combined = namespace_path_mut.clone();
         combined.extend(user_typed_prefix);
         if let Some(namespace) = source.find_child_namespace_by_string_path(&combined) {
-            result.extend(collect_reference_completion_in_namespace(namespace, filter));
+            result.extend(collect_reference_completion_in_namespace(namespace, filter, availability));
         }
         namespace_path_mut.pop();
     }
@@ -38,7 +38,7 @@ fn collect_reference_completion_in_source_internal<'a>(schema: &'a Schema, sourc
                 }
             }
         } else {
-            if filter(top) {
+            if filter(top) && top.available_test(availability) {
                 result.push(top.path().clone())
             }
         }
@@ -47,22 +47,22 @@ fn collect_reference_completion_in_source_internal<'a>(schema: &'a Schema, sourc
         if !examined_sources.contains(&builtin_source.file_path.as_str()) {
             result.extend(collect_reference_completion_in_source_internal(schema, builtin_source, namespace_path, user_typed_prefix, filter, examined_sources, availability));
             if let Some(namespace) = builtin_source.find_child_namespace_by_string_path(&vec!["std"]) {
-                result.extend(collect_reference_completion_in_namespace(namespace, filter));
+                result.extend(collect_reference_completion_in_namespace(namespace, filter, availability));
             }
         }
     }
     result
 }
 
-fn collect_reference_completion_in_namespace(namespace: &Namespace, filter: &Arc<dyn Fn(&Node) -> bool>) -> Vec<Vec<usize>> {
+fn collect_reference_completion_in_namespace(namespace: &Namespace, filter: &Arc<dyn Fn(&Node) -> bool>, availability: Availability) -> Vec<Vec<usize>> {
     let mut result = vec![];
     for top in namespace.children.values() {
         if let Some(namespace) = top.as_namespace() {
-            if namespace.children.values().find(|t| filter(t)).is_some() {
+            if namespace.children.values().find(|t| filter(t) && t.available_test(availability)).is_some() {
                 result.push(namespace.path.clone());
             }
         } else {
-            if filter(top) {
+            if filter(top) && top.available_test(availability) {
                 result.push(top.path().clone())
             }
         }
--- tests/auto_completion/decorator/mod.rs
+++ tests/auto_completion/decorator/mod.rs
@@ -10,4 +10,13 @@ mod test {
         assert_eq!(completions.len(), 1);
         assert_eq!(completions.first().unwrap().label.as_str(), "mygod");
     }
+
+    #[test]
+    fn completion_should_adapt_to_current_availability() {
+        let path_buf = std::env::current_dir().unwrap().join("tests/auto_completion/decorator/schemas/02.teo");
+        let path = path_buf.to_str().unwrap();
+        let (schema, _) = parse(path, None, None);
+        let completions = auto_complete_items(&schema, path, (68, 8));
+        assert_eq!(completions.iter().filter(|c| c.label.as_str() == "db").count(), 1);
+    }
 }
