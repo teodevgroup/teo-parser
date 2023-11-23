55650023d37194738a8cc62beec518c4ec39b422
Victor Teo <victor.teo@fillmula.com>
2023-11-23 11:12:49 +0800

    remove old unused tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -6,48 +6,11 @@ pub mod format;
 mod test {
 
     // #[test]
-    // fn print() {
-    //     let (_, diagnostics) = parse("test.teo", None, None);
-    //     print_to_terminal(&diagnostics);
-    // }
-    //
-    // #[test]
-    // fn lint_to_json() {
-    //     let (_, diagnostics) = parse("/Users/victor/Developer/teo-namespace-example/schema.teo", None, None);
-    //     let result = generate_json_diagnostics(&diagnostics, true);
-    //     println!("{}", result)
-    // }
-    //
-    // #[test]
-    // fn print_dup() {
-    //     let (_, diagnostics) = parse("/Users/victor/Developer/teo-namespace-example/schema.teo", None, None);
-    //     print_to_terminal(&diagnostics)
-    // }
-    //
-    //
-    // #[test]
     // fn test_jump_to_definition() {
     //     let path = "/Users/victor/Developer/teo-namespace-example/part.teo";
     //     let (schema, _) = parse(path, None, None);
     //     let definitions = jump_to_definition(&schema, path, (8, 17));
     //     println!("{:?}", definitions)
-    // }
-    //
-    //
-    // #[test]
-    // fn test_availability() {
-    //     let path = "/Users/victor/Developer/teo-namespace-example/schema.teo";
-    //     let (_schema, _) = parse(path, None, None);
-    //     //let completions = auto_complete_items(&schema, path, (4, 13));
-    //     //println!("{:?}", completions);
-    // }
-    //
-    // #[test]
-    // fn test_shape_resolve() {
-    //     let path = "/Users/victor/Developer/teo-namespace-example/shape.teo";
-    //     let (_schema, _) = parse(path, None, None);
-    //     //let completions = auto_complete_items(&schema, path, (4, 13));
-    //     //println!("{:?}", completions);
     // }
     //
     // use backtrace_on_stack_overflow;
