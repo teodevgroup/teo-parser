050c39f5d56a2a4b3bb99683576f13660ae29348
Victor Teo <victor.teo@fillmula.com>
2023-11-24 11:45:38 +0800

    jump to definition in constant type expr

--- src/definition/jump_to_definition_in_constant.rs
+++ src/definition/jump_to_definition_in_constant.rs
@@ -3,8 +3,10 @@ use crate::ast::schema::Schema;
 use crate::ast::source::Source;
 use crate::definition::definition::Definition;
 use crate::definition::jump_to_definition_in_expression::jump_to_definition_in_expression;
+use crate::definition::jump_to_definition_in_type_expr::jump_to_definition_in_type_expr_kind;
 use crate::r#type::r#type::Type;
 use crate::search::search_availability::search_availability;
+use crate::traits::info_provider::InfoProvider;
 use crate::traits::resolved::Resolve;
 use crate::traits::node_trait::NodeTrait;
 
@@ -14,10 +16,23 @@ pub(super) fn jump_to_definition_in_constant<'a>(
     constant: &'a ConstantDeclaration,
     line_col: (usize, usize),
 ) -> Vec<Definition> {
-    let mut namespace_path: Vec<&str> = constant.string_path.iter().map(|s| s.as_str()).collect();
-    namespace_path.pop();
+    let namespace_path: Vec<&str> = constant.namespace_str_path();
     let availability = search_availability(schema, source, &namespace_path);
-    if constant.expression().span().contains_line_col(line_col) {
+    if let Some(type_expr) = constant.type_expr() {
+        if type_expr.span().contains_line_col(line_col) {
+            jump_to_definition_in_type_expr_kind(
+                schema,
+                source,
+                &type_expr.kind,
+                &namespace_path,
+                line_col,
+                &vec![],
+                availability,
+            )
+        } else {
+            vec![]
+        }
+    } else if constant.expression().span().contains_line_col(line_col) {
         let undetermined = Type::Undetermined;
         return jump_to_definition_in_expression(
             schema,
