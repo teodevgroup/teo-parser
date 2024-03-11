9bc19562e9f10e88e2f8e49546268e4c3ab899eb
Victor Teo <victor.teo@fillmula.com>
2024-03-11 23:58:22 +0800

    fix

--- src/resolver/resolve_include_handler_from_template.rs
+++ src/resolver/resolve_include_handler_from_template.rs
@@ -1,10 +1,12 @@
 use maplit::btreemap;
 use crate::ast::include_handler_from_template::{IncludeHandlerFromTemplate, IncludeHandlerFromTemplateResolved};
 use crate::ast::model::Model;
+use crate::ast::reference_space::ReferenceSpace;
 use crate::availability::Availability;
 use crate::r#type::keyword::Keyword;
 use crate::r#type::reference::Reference;
 use crate::r#type::Type;
+use crate::resolver::resolve_decorator::resolve_decorator;
 use crate::resolver::resolve_identifier::{resolve_identifier_path_names_with_filter_to_top};
 use crate::resolver::resolver_context::ResolverContext;
 use crate::traits::node_trait::NodeTrait;
@@ -41,13 +43,18 @@ pub(super) fn resolve_include_handler_from_template_decorators<'a>(
         let output_type = template.output_type().resolved();
         resolved.output_type = type_replace_generics_for_handler_template_type(output_type, model);
         include_handler_from_template.resolve(resolved);
+        let mut keywords_map = btreemap!{};
+        keywords_map.insert(Keyword::SelfIdentifier, Type::ModelObject(Reference::new(model.path.clone(), model.string_path.clone())));
+        for decorator in include_handler_from_template.decorators() {
+            resolve_decorator(decorator, context, &keywords_map, ReferenceSpace::HandlerDecorator);
+        }
     } else {
         context.insert_diagnostics_error(include_handler_from_template.identifier_path().span(), "handler template definition is not found");
     }
 }
 
 fn type_replace_generics_for_handler_template_type(original: &Type, model: &Model) -> Type {
-    original.replace_keywords(btreemap! {
+    original.replace_keywords(&btreemap! {
         Keyword::SelfIdentifier => Type::ModelObject(Reference::new(model.path.clone(), model.string_path.clone())),
     })
 }
